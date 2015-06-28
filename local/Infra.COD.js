7d190b83da25f67958ea538936be659beed46e59
Huang Yonggang <huangyg@mars22.com>
2015-06-28 18:25:10 +0800

    cod

---
+++ local/Infra.COD.js
@@ -0,0 +1,45 @@
+var https = require('https');
+var fs = require('fs');
+var events = require('events');
+var emitter = new events.EventEmitter();
+
+var indexurl = "";
+var cod ;
+https.get(indexurl,function (response){
+	response.on('data',function(data){
+		cod = yaml.safeLoad(data);
+	});
+});
+
+for (var i in cod) {
+	console.log(cod[i]);
+	event = cod[i].event;
+	for (var id in event) {
+		console.log(id+event[id]);
+	}
+}
+/*
+var ticket = require('./ticket')
+
+emitter.on("ticket1",eval("ticket.t1"));
+emitter.on("ticket2",ticket.t2);
+emitter.emit("ticket1");
+
+/*
+https.get("https://raw.githubusercontent.com/hyg/js.sample/master/learnyounode/6.module.js",function (response){
+	response.on('data',function(data){
+		//console.log(data.toString());
+		fs.writeFile("a.js",data,function(err){
+			var a = require("./a.js");
+			a("C:/Users/huangyg/Desktop","pdf",function callback(err, data){
+				if (err) throw err;
+				
+				for(var i=0;i<data.length;i++){
+					console.log(data[i].toString())
+				}
+			});
+		});
+	});
+	
+});
+*/
