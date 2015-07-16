7abbd0ff7bf8bf9ec1e4bb6caa2527bdb09e7920
Huang Yonggang <huangyg@mars22.com>
2015-07-16 17:29:48 +0800

    sync

--- local/Infra.js
+++ local/Infra.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var yaml = require('js-yaml');
 var http = require('http');
+var https = require('https');
+var async = require('async');
 var openpgp = require('openpgp');
 var events = require('events');
 
@@ -15,7 +17,7 @@ exports.getCODObj = getCODObj ;
 
 exports.createNor = createNor ;
 exports.sent = sent ;
-exports.sync = sync ;
+exports.postsync = postsync ;
 
 
 function getCODlist(){
@@ -65,6 +67,12 @@ function createNor(name,id,email,passphrase,callback){
 }
 
 // distribute storage
+var localPostIdx = yaml.safeLoad(fs.readFileSync('post/index.yaml', 'utf8'));
+var localPutIdx = yaml.safeLoad(fs.readFileSync('put/index.yaml', 'utf8'));
+var globalPostIdx ,globalPutIdx;
+var postfileArray = new Array() ;
+var putfileArray = new Array() ;
+
 function sent(item,method,callback){
 	var itemyaml = yaml.safeDump(item);
 	var options = {
@@ -93,9 +101,124 @@ function sent(item,method,callback){
 	req.end();
 }
 
-function sync() {
+function postsync(finish) {
+	var addr = "http://"+config.server.url+":"+config.server.port+'/post/index.yaml';
+	var req = http.get(addr, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			globalPostIdx = yaml.safeLoad(chunk);
+			for (var key in globalPostIdx) {
+				//console.log("key:\t"+key);
+				if (key == "update") continue;
+				if (!localPostIdx.hasOwnProperty(key)) {
+					localPostIdx[key] = 0;
+				}
+				//console.log(localPostIdx[key]);
+				//console.log(globalPostIdx[key]);
+				if (localPostIdx[key] < globalPostIdx[key]){
+					for (var id = localPostIdx[key]+1;id <= globalPostIdx[key];id++) {
+						//console.log("key:\t"+key+"\tid:\t"+id);
+						
+						postfileArray.push(key+"."+id.toString()+".yaml") ;
+					}
+					localPostIdx[key] = globalPostIdx[key];
+				}
+			}
+			
+			//console.log(postfileArray);
+			
+			var createtime = new Object();
+			
+			async.each(postfileArray, function (item, callback) {
+				var fileaddr = "http://"+config.server.url+":"+config.server.port+'/post/'+item;
+				var filename = "post/"+item;
+				var req = http.get(fileaddr, function(res) {
+					res.setEncoding('utf8');
+					res.on('data', function (chunk) {
+						fs.writeFileSync(filename,chunk);
+						console.log("post: "+filename+" saved.");
+						
+						// parse the yaml and get the createat field
+						// write into a object: [createat]filename
+						var itemdata = yaml.safeLoad(chunk);
+						createtime[itemdata.createat] = item ;
+						
+						callback();
+					});
+				}).on('error', function(e) {
+					console.log('problem with request: ' + e.message);
+				});
+			}, function (err) {
+				if( err ) {
+					console.log('post:A file failed to save');
+				} else {
+					// sort the object and emit event one by one
+					var sortedcreatetime = sortObject(createtime);
+					console.log(sortedcreatetime);
+					
+					for (var time in sortedcreatetime) {
+						var item = sortedcreatetime[time];
+						emitter.emit("postfile",item);
+					}
+					localPostIdx.update = new Date().toLocaleString();
+					fs.writeFileSync("post/index.yaml",yaml.safeDump(localPostIdx));
+					//console.log('post:All files have been saved successfully');
+					if (typeof(finish) != "undefined") {
+						finish();
+					}
+				}
+			});
+		});
+	}).on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	});
 }
 
 
 
 // distribute event driver
+emitter.on("postfile",function(item){
+	console.log("event postfile, item: ",item);
+	if((item.substr(item.indexOf(".")+1,5) == "auto.") || (item.substr(0,5) == "auto.")){
+		var auto = yaml.safeLoad(fs.readFileSync("post/"+item, 'utf8'));
+		var autofilename = item.substr(0,item.lastIndexOf(".")) + ".js" ;
+		
+		console.log("new auto account: download "+auto.data.codeurl+" and saved as "+autofilename);
+		var autoget = https.get(auto.data.codeurl,function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				fs.writeFileSync(autofilename,chunk);
+				
+				var a = require("./"+autofilename);
+				for (var event in auto.data.listener){
+					var lf = auto.data.listener[event] ;
+					//console.log("a."+lf);
+					emitter.on(event,eval("a."+lf));
+					console.log(emitter);
+				}
+			});
+		});
+	}
+})
+
+
+
+//utility
+
+function sortObject(o) {
+    var sorted = {},
+    key, a = [];
+
+    for (key in o) {
+        if (o.hasOwnProperty(key)) {
+            a.push(key);
+        }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+        sorted[a[key]] = o[a[key]];
+    }
+    return sorted;
+}
