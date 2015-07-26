2e7a9401041041883c4d595a21eb508098cec7b3
Huang Yonggang <huangyg@xuemen.com>
2015-07-26 22:08:36 +0800

    add server

---
+++ server/ITW.js
@@ -0,0 +1,47 @@
+var os = require('os');
+var child  =  require('child_process');
+var fs = require('fs');
+var Hashes = require('jshashes');
+var events = require('events');
+var emitter = new events.EventEmitter();
+
+
+exports.t1 = t1;
+function t1(stream){
+	console.log(getHASH());
+	openbrowser("http://www.xuemen.com");
+}
+
+exports.t2 = t2;
+
+function t2(stream){
+	openbrowser("http://www.baidu.com");
+}
+
+function openbrowser(url) {
+	switch (os.platform()) {
+	case "linux":
+		child.exec("xdg-open "+ url);
+		break;
+	case "win32":
+	case "win64":
+		//child.spawnSync("rundll32", "url.dll,FileProtocolHandler", url);
+		//child.exec("start", url);
+		child.exec("start "+url);
+		break;
+	case "darwin":
+		child.exec("open "+ url);
+		break;
+	default:
+		console.log("unsupported platform");
+		break;
+	};
+}
+
+function getHASH(){
+	var filename = "ITW.js";
+	var data = fs.readFileSync(filename);
+	var datahash = new Hashes.SHA512().b64(data.toString())
+	
+	return datahash;
+}
---
+++ server/Infra.data.js
@@ -0,0 +1,154 @@
+
+var fs = require('fs');
+var http = require('http');
+
+var url = require("url"),
+	yaml = require('js-yaml');
+
+var PostIdx,PutIdx;
+	
+if (!fs.existsSync("post")) {
+	fs.mkdirSync("post", 0777)
+};
+if (fs.existsSync("post/index.yaml")) {
+	PostIdx = yaml.safeLoad(fs.readFileSync('post/index.yaml', 'utf8'));
+}else {
+	PostIdx = new Object();
+	PostIdx.update = new Date().toLocaleString();
+	fs.writeFileSync("post/index.yaml",yaml.safeDump(PostIdx));
+};
+
+if (!fs.existsSync("put")) {
+	fs.mkdirSync("put", 0777)
+};
+if (fs.existsSync("put/index.yaml")) {
+	PutIdx = yaml.safeLoad(fs.readFileSync('put/index.yaml', 'utf8'));
+}else {
+	PutIdx = new Object();
+	PutIdx.update = new Date().toLocaleString();
+	fs.writeFileSync("put/index.yaml",yaml.safeDump(PutIdx));
+};
+
+var server = http.createServer(function (req, res) {
+	var chunk = ""; 
+	req.on('data', function(data){
+      chunk += data ;
+    });
+	req.on('end', function(){
+		if(req.method == 'POST') {
+			console.log('BODY: ' + chunk);
+			console.log('BODY length: ' + chunk.length);
+			var body = yaml.load(chunk);
+			
+			if (body.hasOwnProperty("cod")) {
+				key = body.cod + "." + body.tag + "." + body.author;
+			} else {
+				key = body.tag + "." + body.author;
+			}
+			if (!PostIdx.hasOwnProperty(key)) {
+				PostIdx[key] = 0;
+				PostIdx.update = new Date().toLocaleString();
+				fs.writeFile("post/index.yaml",yaml.safeDump(PostIdx),function(err){
+					console.log("post notify: create a new key ["+key+"].\n");
+				});
+			}
+			
+			var filename;
+			PostIdx[key] = PostIdx[key] + 1;
+			if (body.hasOwnProperty("cod")) {
+				filename = "post/" + body.cod + "." + body.tag + "." + body.author + "." + (PostIdx[key]) + ".yaml";
+			} else {
+				filename = "post/" + body.tag + "." + body.author + "." + (PostIdx[key]) + ".yaml";
+			}
+			fs.exists(filename, function (exists) {
+				if (exists) {
+					res.writeHead(400, {'Content-Type': 'text/plain'});
+					res.write( "post fail: file "+filename+" exist.");
+					res.end();
+					console.log("post fail: file "+filename+" exist.");
+				} else {
+					body.createat = new Date().getTime();
+					fs.writeFile(filename,yaml.safeDump(body),function(err){
+						if(err) throw err;
+						res.writeHead(201, {'Content-Type': 'text/plain'});
+						res.write( key  + "." + (PostIdx[key]+1) );
+						res.end();
+						console.log("post: "+filename+" saved.");
+						
+						//PostIdx[key] = PostIdx[key] + 1;
+						PostIdx.update = new Date().toLocaleString();
+						fs.writeFileSync("post/index.yaml",yaml.safeDump(PostIdx));
+					});
+				}
+			})
+		} 
+		if(req.method == 'PUT') {
+			console.log('BODY: ' + chunk);
+			var body = yaml.load(chunk);
+			
+			var filename;
+			if (body.hasOwnProperty("cod")) {
+				filename = body.cod + "." + body.tag + "." + body.author ;
+			} else {
+				filename = body.tag + "." + body.author ;
+			}
+			if (body.hasOwnProperty("index")) {
+				filename = filename + "." + body.index + ".yaml";
+			} else {
+				filename = filename + ".yaml";
+			}
+			
+			fs.exists(filename, function (exists) {
+				if (exists) {
+					res.writeHead(202, {'Content-Type': 'text/plain'});
+					res.write( "put: "+filename+" updated.");
+					res.end();
+					console.log("put: "+filename+" updated.");
+				} else {
+						res.writeHead(201, {'Content-Type': 'text/plain'});
+						res.write( "put: "+filename+" saved.");
+						console.log("put: "+filename+" saved.");
+						res.end();
+				}
+			});
+				
+			fs.writeFile("put/" + filename,yaml.safeDump(body),function(err){
+				if(err) throw err;
+
+				PutIdx[filename] = new Date().toLocaleString();
+				PutIdx.update = new Date().toLocaleString();
+				fs.writeFileSync("put/index.yaml",yaml.safeDump(PutIdx));
+			});
+		} 
+		if(req.method == 'GET') {
+			var pathname = url.parse(req.url).pathname;
+			var realPath = pathname.substring(1);
+			console.log(realPath);
+			fs.exists(realPath, function (exists) {
+				if (!exists) {
+					res.writeHead(404, {'Content-Type': 'text/plain'});
+					res.write("访问路径没有找到。","utf8");
+					res.end();
+				} else {
+					//fs.createReadStream(realPath).pipe(res)
+					
+					var options = {encoding:'utf8'}
+					
+					fs.readFile(realPath,options, function(err, file){    
+						if ( err ) {    
+							res.writeHead(500, {'Content-Type': 'text/plain'});    
+							// res.write();    
+							res.end();    
+						} else {
+							console.log(file);
+							res.writeHead(200, {'Content-Type':'text/plain'});    // or text/x-yaml  to make client save a file
+							res.write(file);    
+							res.end();    
+						}
+					});
+				}
+			})
+		}
+    })
+});
+server.listen(46372);
