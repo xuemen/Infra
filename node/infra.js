2858d134e57c5b563edd856bf601c1d55abd8f66
Huang Yonggang <huangyg@mars22.com>
2016-02-10 03:11:14 +0800

    重写事件驱动机制前存一下。

--- node/infra.js
+++ node/infra.js
@@ -5,10 +5,7 @@ var yaml = require('js-yaml');
 
 var log,cfg ;
 init();
-//writedata("/huangyg/itw/admin/","test","log")
-for (var event in yaml){
-	console.log("yaml> var:",event);
-}
+writedata("/huangyg/itw/key/","test","cfg");
 
 
 
@@ -36,23 +33,38 @@ function eventloop(){
 function writedata(k,v,type){
 	switch(type){
 		case "log":
-			log[k] = v;
-			fs.writeFile("log.yaml",yaml.safeDump(log),function(err){
-					console.log("log file wrote.\n");
-				});
+			if (log[k] === undefined){
+				log[k] = v;
+				log.update = new Date().toLocaleString();
+				fs.writeFileSync("log.yaml",yaml.safeDump(log));
+				console.log("log file wrote:\n",yaml.safeDump(log));
+			} else {
+				console.log(k,"existed in log file.\n");
+			}
+			break;			
+		case "cfg":
+			cfg[k] = v;
+			cfg.update = new Date().toLocaleString();
+			fs.writeFileSync("cfg.yaml",yaml.safeDump(cfg));
+			console.log("cfg file wrote:\n",yaml.safeDump(cfg));
 			break;
 	}
-	
 }
 
-function readdata(k) {
-	
+function readdata(k,type) {
+	switch(type){
+		case "log":
+			return log[k];
+		case "cfg":
+			return cfg[k];
+	}
 }
 
 function deletedata(k){
-	
+	delete cfg[k];
+	fs.writeFileSync("cfg.yaml",yaml.safeDump(cfg));
+	console.log("cfg file wrote:\n",yaml.safeDump(cfg));
 }
-
 
 function init(){
 	if (fs.existsSync("log.yaml")) {
@@ -71,5 +83,27 @@ function init(){
 		
 		fs.writeFileSync("log.yaml",yaml.safeDump(log));
 	};
+	
+	if (fs.existsSync("cfg.yaml")) {
+		cfg = yaml.safeLoad(fs.readFileSync('cfg.yaml', 'utf8'));
+		// check the old version files, and transfer to new version
+		for (var key in cfg) {
+			//console.log("key:\t"+key);
+			if (key === "updateLocal") continue;
+			if (key === "update") continue;
+			
+			}
+	}else {
+		cfg = new Object();
+		cfg.update = new Date().toLocaleString();
+		console.log("cfg:\t"+cfg);
+		
+		fs.writeFileSync("cfg.yaml",yaml.safeDump(cfg));
+	};
 }
 
+
+// sync 
+function sync(){
+	
+}
